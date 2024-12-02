61e6ec0f91a3f708a8232e1bebaa2dcd0acbf2a7
vangxandy <102646547+vangxandy@users.noreply.github.com>
2024-12-02 14:16:05 -0600

    Update validate-login.php

--- validate-login.php
+++ validate-login.php
@@ -55,10 +55,14 @@ $conn->close();
     <title>Blog ABCD</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
-
-<h1>ABCD BLOG Sign-In Page</h1>
-
+<main>
+<header>
+    <a href="index.php">
+        <img src="images/abcd.png" alt="ABCD Blog Logo">
+    </a>
+</header>
 
 <body>
     <nav>
@@ -66,25 +70,29 @@ $conn->close();
             <li><a href="index.php">Home</a></li>
             <li><a href="sign-in.php">Sign In</a></li>
             <li><a href="create-account.php">Create Account</a></li>
+            <li><a href="create_blog.php">Create Blog Post</a></li>
         </ul>
     </nav>
-
-    <form action="validate-login.php" method="post">
-        <div>
+    <div id="sign-in-form">
+        <form action="validate-login.php" method="post">
+            <h2>Sign-In</h2>
             <label for="usermail">Email:</label>
+            <br>
             <input type="email" id="usermail" name="usermail" placeholder="example@gmail.com" required>
-        </div>
-        <br>
-        <div>
             <label for="password">Password:</label>
+            <br>
             <input type="password" id="password" name="password" placeholder="Password" required>
-        </div>
-        <br>
-        <input type="submit" id="submit-login" value="Login"/>
-    </form>
+            <br>
+            <input type="submit" id="submit-login" value="Login"/>
+            <br>
+        </form>
+        <p>Invalid email or password.</p>
+    </div>
 
-
-    
+    </main>
     <script src="script.js"></script>
+    <footer>
+        <p>© 2024 ABCD Blog. All rights reserved.</p>
+    </footer>
 </body>
 </html>
