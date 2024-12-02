1ea1d70967ad2f90f3ad7cd2b83f4410435a2994
vangxandy <102646547+vangxandy@users.noreply.github.com>
2024-12-02 14:17:56 -0600

    Update create-account.php

--- create-account.php
+++ create-account.php
@@ -14,11 +14,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog ABCD</title>
     <link rel="stylesheet" href="styles.css">
+    <style>
+
+    </style>
 </head>
+
+
 <body>
-
-<h1>ABCD BLOG Create-Account Page</h1>
-
+<main>
+<header>
+    <a href="index.php">
+        <img src="images/abcd.png" alt="ABCD Blog Logo">
+    </a>
+</header>
 
 <body>
     <nav>
@@ -26,39 +34,45 @@
             <li><a href="index.php">Home</a></li>
             <li><a href="sign-in.php">Sign In</a></li>
             <li><a href="create-account.php">Create Account</a></li>
+            <li><a href="create_blog.php">Create Blog Post</a></li>
         </ul>
     </nav>
 
 
 
-
-    <form action="process_new_account.php" method="post">
-      <label for="firstname">First Name</label>
-      <br>
-      <input id="firstname" type="text" name="firstname" placeholder="First Name" required>
-      <br>
-      <label for="lastname">Last Name</label>
-      <br>
-      <input id="lastname" type="text" name="lastname" placeholder="Last name" required>
-      <br>
-      <label for="usermail">Email</label>
-      <br>
-      <input id="usermail" type="email" name="usermail" placeholder="Yourname@email.com" required>
-      <br>
-      <label for="password">Password</label>
-      <br>
-      <input id="password" type="password" name="password" placeholder="Password" required>
-      <br>
-      <input type="submit" id="submit-login" value="Create Account"/>
-	</form>
-
-
-
-
-
+    <div id="create-account-form">
+      <h2>Create-Account Form</h2>
+      <form action="process_new_account.php" method="post">
+        <label for="firstname">First Name:</label>
+        <br>
+        <input id="firstname" type="text" name="firstname" placeholder="First Name" required>
+        <br>
+        <label for="lastname">Last Name:</label>
+        <br>
+        <input id="lastname" type="text" name="lastname" placeholder="Last name" required>
+        <br>
+        <label for="usermail">Email:</label>
+        <br>
+        <input id="usermail" type="email" name="usermail" placeholder="Yourname@email.com" required>
+        <br>
+        <label for="password">Password:</label>
+        <br>
+        <input id="password" type="password" name="password" placeholder="Password" required>
+        <br>
+        <input type="submit" id="submit-login" value="Create Account"/>
+        <br>
+    </form>
+  </div>
 
 
-    
+
+
+
+
+  </main>
     <script src="script.js"></script>
+    <footer>
+        <p>© 2024 ABCD Blog. All rights reserved.</p>
+    </footer>
 </body>
 </html>
