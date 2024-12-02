e5dd3f076a28bce7838618431aeb208e70297293
vangxandy <102646547+vangxandy@users.noreply.github.com>
2024-12-02 15:50:38 -0600

    Update delete_blog.php

--- delete_blog.php
+++ delete_blog.php
@@ -17,14 +17,12 @@
 </header>
 
 <body>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="sign-in.php">Sign In</a></li>
-            <li><a href="create-account.php">Create Account</a></li>
-            <li><a href="create_blog.php">Create Blog Post</a></li>
-        </ul>
-    </nav>
+<nav>
+    <ul>
+        <li><a href="index.php">Home/Log-Out</a></li>
+        <li><a href="create_blog.php">Create Blog Post</a></li>
+    </ul>
+</nav>
 
 
 <?php
